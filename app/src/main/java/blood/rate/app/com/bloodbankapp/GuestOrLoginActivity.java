7a4d224cb00e8d040deb386d4dbb9d50534b9d9f
sagarsrao <sagar.s.rao@gmail.com>
2017-06-21 09:18:25 +0530

    Initial commit

---
+++ app/src/main/java/blood/rate/app/com/bloodbankapp/GuestOrLoginActivity.java
@@ -0,0 +1,16 @@
+package blood.rate.app.com.bloodbankapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuestOrLoginActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_or_login);
+    }
+}
